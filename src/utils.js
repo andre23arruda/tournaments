3a98434cfbe2340250dc5a0fd248d7c1819e3d93
andre23arruda <andre23arruda@gmail.com>
2025-08-28 10:01:41 -0300

    feat: Improve date formatting function to handle date strings correctly

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 function formatDate(dateString) {
-    return new Date(dateString).toLocaleDateString('pt-BR', {
+    const [year, month, day] = dateString.split('-');
+    return new Date(year, month - 1, day).toLocaleDateString('pt-BR', {
         year: 'numeric',
         month: '2-digit',
         day: '2-digit'
