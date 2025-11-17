162c2ff69caf2ee896272b435a7a84d80d02b90c
andre23arruda <andre23arruda@gmail.com>
2025-11-17 09:09:20 -0300

    feat: Add speed-insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import League from './League';
 import LeagueRanking from './League/Ranking';
 import Main from './Main';
@@ -27,8 +26,6 @@ const App = () => {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-
-      <SpeedInsights />
     </React.Fragment>
   );
 };
