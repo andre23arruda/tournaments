46cdef3e04096706ab2f8dba7daef6bd578a8e53
andre23arruda <andre23arruda@gmail.com>
2026-02-17 10:56:26 -0300

    feat: Update playes inline text

--- public/admin_assets/js/players-inline-text.js
+++ public/admin_assets/js/players-inline-text.js
@@ -3,6 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const isActive = document.querySelector('#id_ativo').checked
     if (playersInline && isActive) {
         const html = playersInline.innerHTML
-        playersInline.innerHTML = '<h6 class="text-center mt-1">Verifique se o jogador já está cadastrado no sistema.<br/><br/>O número de jogadores deve ser múltiplo de 4.</h6>' + html
+        playersInline.innerHTML = '<h6 class="text-center mt-1">Verifique se o jogador já está cadastrado no sistema.</h6>' + html
     }
 })
